62cdcc9f166aeb9554f7c877980c9a0b916c92c1
satishary <88817645+satishary@users.noreply.github.com>
2022-09-26 18:45:56 +0530

    Multiple Catch Blocks

--- src/exceptionhandling/HandlerMain.java
+++ src/exceptionhandling/HandlerMain.java
@@ -1,21 +1,22 @@
-/*
-Satish Aryan Zindabad
- */
 package exceptionhandling;
 
-/**
- *
- * try, catch, exception, throw, throws and finally
- */
 public class HandlerMain {
 
     public static void main(String[] args) {
         try {
-            int n = 0;
-            int m = 1 / n;
-            System.out.println(m);
-
+            int n = 70;
+            n = 1 / n;
+            int[] a = {78};
+            a[0] = 9;
+            System.out.println("No Error");
+        } catch (ArithmeticException ex) {
+            System.out.println("Arithmetic Error");
+            System.out.println(ex);
+        } catch (NullPointerException ex) {
+            System.out.println("Null Pointer Error");
+            System.out.println(ex);
         } catch (Exception ex) {
+            System.out.println("Unknown Error");
             System.out.println(ex);
 
         } finally {
